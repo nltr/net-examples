f6542aeb7db98c22d9142d903977cffb6bfe8d0f
nltr <learning@inishant.in>
2019-11-23 17:50:33 +0530

    changing custom startup as hello world

--- webapp1/Program.cs
+++ webapp1/Program.cs
@@ -20,7 +20,7 @@ namespace webapp1
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<MyStartup>();
                 });
     }
 }
